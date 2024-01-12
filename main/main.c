e20448a505b6db1f2f13664e8f9748a3de55a29e
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-01-12 12:21:55 +0900

    added  mdns_service_add

--- main/main.c
+++ main/main.c
@@ -1,9 +1,10 @@
 /*
-	 Serial monitor client example using WEB Socket.
-	 This example code is in the Public Domain (or CC0 licensed, at your option.)
-	 Unless required by applicable law or agreed to in writing, this
-	 software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-	 CONDITIONS OF ANY KIND, either express or implied.
+	Serial monitor client example using WEB Socket.
+
+	This example code is in the Public Domain (or CC0 licensed, at your option.)
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
 */
 
 #include <stdio.h>
@@ -147,6 +148,8 @@ void initialise_mdns(void)
 	ESP_ERROR_CHECK( mdns_hostname_set(CONFIG_MDNS_HOSTNAME) );
 	ESP_LOGI(TAG, "mdns hostname set to: [%s]", CONFIG_MDNS_HOSTNAME);
 
+	//initialize service
+	ESP_ERROR_CHECK( mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0) );
 #if 0
 	//set default mDNS instance name
 	ESP_ERROR_CHECK( mdns_instance_name_set("ESP32 with mDNS") );
